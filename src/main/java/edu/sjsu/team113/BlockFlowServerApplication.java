8e71ae5e455bda207836cceed9df9128bdf4667d
Nikhil <nikstvp@gmail.com>
2016-04-24 22:34:59 -0700

    hibernate session bean configured

--- src/main/java/edu/sjsu/team113/BlockFlowServerApplication.java
+++ src/main/java/edu/sjsu/team113/BlockFlowServerApplication.java
@@ -2,8 +2,10 @@ package edu.sjsu.team113;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -12,5 +14,10 @@ public class BlockFlowServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlockFlowServerApplication.class, args);
+	}
+	
+	@Bean
+	public HibernateJpaSessionFactoryBean sessionFactory() {
+	    return new HibernateJpaSessionFactoryBean();
 	}
 }
